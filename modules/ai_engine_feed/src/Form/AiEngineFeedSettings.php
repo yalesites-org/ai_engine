0b449fbc0e6aed57706a7fb4b2060d3035a35901
David Blankenship <david.blankenship@yale.edu>
2024-06-13 08:36:00 -0400

    feat(YSP-539): select from available metatags

--- modules/ai_engine_feed/src/Form/AiEngineFeedSettings.php
+++ modules/ai_engine_feed/src/Form/AiEngineFeedSettings.php
@@ -35,11 +35,20 @@ class AiEngineFeedSettings extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config(self::CONFIG_NAME);
+    $possibleMetatags = $this->getAllMetatagsFields();
+    $defaultMetatag = "field_metatags";
+
+    $defaultValue = '';
+
+    if (isset($possibleMetatags[$defaultMetatag])) {
+      $defaultValue = $defaultMetatag;
+    }
 
     $form['metatags_field'] = [
-      '#type' => 'textfield',
+      '#type' => 'select',
+      '#options' => $this->getAllMetatagsFields(),
       '#title' => $this->t('Metatags field'),
-      '#default_value' => 'field_metatags',
+      '#default_value' => $config->get('metatags_field') ?? $defaultValue,
       '#description' => $this->t('The field name for storing metatags related to AI.'),
     ];
 
@@ -55,7 +64,7 @@ class AiEngineFeedSettings extends ConfigFormBase {
     if (!empty($metatags_field)) {
       $field_storage = \Drupal::entityTypeManager()->getStorage('field_storage_config')->load('node.' . $metatags_field);
       if (empty($field_storage)) {
-      $form_state->setErrorByName('metatags_field', $this->t('Field @field does not exist.', ['@field' => $metatags_field]));
+        $form_state->setErrorByName('metatags_field', $this->t('Field @field does not exist.', ['@field' => $metatags_field]));
       }
     }
 
@@ -70,6 +79,30 @@ class AiEngineFeedSettings extends ConfigFormBase {
       ->set('metatags_field', $form_state->getValue('metatags_field'))
       ->save();
     parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * Get all metatags to display to the user.
+   */
+  protected function getAllMetatagsFields() {
+    $fields = $this->appendNoMetatagEntry([]);
+    $metatagsList = \Drupal::service('entity_field.manager')->getFieldMapByFieldType('metatag');
+
+    foreach ($metatagsList as $entity_type => $fieldsList) {
+      foreach ($fieldsList as $field_name => $field) {
+        $fields[$field_name] = $field_name;
+      }
+    }
+
+    return $fields;
+  }
+
+  /**
+   * Append the no metatag entry to the list of metatags.
+   */
+  protected function appendNoMetatagEntry($fields) {
+    $fields[''] = 'Do not use metatags';
+    return $fields;
   }
 
 }
