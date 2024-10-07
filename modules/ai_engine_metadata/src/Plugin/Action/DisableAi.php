f8811b9cb90e710492678b31a4d0e9c2d1fbaacb
David Blankenship <david.blankenship@yale.edu>
2024-10-07 09:57:29 -0400

    feat(YSP-672): add enable/disable ai bulk actions
    Still work in progress, but would give the ability to "opt-in" or out of
    AI inclusion hopefully in many different content types.

---
+++ modules/ai_engine_metadata/src/Plugin/Action/DisableAi.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\ai_engine_metadata\Plugin\Action;
+
+use Drupal\Core\Access\AccessResultInterface;
+use Drupal\Core\Action\ActionBase;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Provides a Disable AI action.
+ *
+ * @Action(
+ *   id = "ai_engine_disable_ai",
+ *   label = @Translation("Disable AI"),
+ *   type = "node",
+ *   category = @Translation("Custom"),
+ * )
+ *
+ * @DCG
+ * For updating entity fields consider extending FieldUpdateActionBase.
+ * @see \Drupal\Core\Field\FieldUpdateActionBase
+ *
+ * @DCG
+ * In order to set up the action through admin interface the plugin has to be
+ * configurable.
+ * @see https://www.drupal.org/project/drupal/issues/2815301
+ * @see https://www.drupal.org/project/drupal/issues/2815297
+ *
+ * @DCG
+ * The whole action API is subject of change.
+ * @see https://www.drupal.org/project/drupal/issues/2011038
+ */
+final class DisableAi extends ActionBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function access($entity, ?AccountInterface $account = NULL, $return_as_object = FALSE): AccessResultInterface|bool {
+    /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
+    $access = $entity->access('update', $account, TRUE);
+    /*->andIf($entity->metatag->ai_disable_indexing->access('edit', $account, TRUE));*/
+    return $return_as_object ? $access : $access->isAllowed();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute(?ContentEntityInterface $entity = NULL): void {
+    $entity->metatag->ai_disable_indexing = 'disabled';
+    $entity->save();
+  }
+
+}
