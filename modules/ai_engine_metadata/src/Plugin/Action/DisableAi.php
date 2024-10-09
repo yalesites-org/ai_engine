9f57aa8827a924235418b7264ce69117d868d08b
David Blankenship <david.blankenship@yale.edu>
2024-10-09 11:46:09 -0400

    refactor(YSP-672): generalize actions for code reuse

--- modules/ai_engine_metadata/src/Plugin/Action/DisableAi.php
+++ modules/ai_engine_metadata/src/Plugin/Action/DisableAi.php
@@ -4,12 +4,6 @@ declare(strict_types=1);
 
 namespace Drupal\ai_engine_metadata\Plugin\Action;
 
-use Drupal\Core\Access\AccessResult;
-use Drupal\Core\Access\AccessResultInterface;
-use Drupal\Core\Action\ActionBase;
-use Drupal\Core\Entity\ContentEntityInterface;
-use Drupal\Core\Session\AccountInterface;
-
 /**
  * Provides a Disable AI action.
  *
@@ -34,37 +28,20 @@ use Drupal\Core\Session\AccountInterface;
  * The whole action API is subject of change.
  * @see https://www.drupal.org/project/drupal/issues/2011038
  */
-class DisableAi extends ActionBase {
-
-  const METATAG_FIELD_NAME = 'ai_disable_indexing';
-  const ACTION_VALUE = 'disabled';
-  const MANAGE_AI_PERMISSION = 'manage ai engine settings';
+class DisableAi extends MetatagValueSetAction {
+  /**
+   * {@inheritdoc}
+   */
+  protected static $entityMetatagFieldName = 'ai_disable_indexing';
 
   /**
    * {@inheritdoc}
    */
-  public function access($entity, ?AccountInterface $account = NULL, $return_as_object = FALSE): AccessResultInterface|bool {
-    /** @var \Drupal\Core\Entity\ContentEntityInterface $entity */
-    $access = $entity->access('update', $account, TRUE)
-      ->andIf(AccessResult::allowedIfHasPermission($account, self::MANAGE_AI_PERMISSION));
-    return $return_as_object ? $access : $access->isAllowed();
-  }
+  protected static $metatagFieldName = 'field_metatags';
 
   /**
    * {@inheritdoc}
    */
-  public function execute(?ContentEntityInterface $entity = NULL): void {
-    if (!$entity) {
-      return;
-    }
-
-    if ($entity->hasField('field_metatags')) {
-      $metaTagsArray = json_decode($entity->field_metatags->value ?? "{}", TRUE);
-      $metaTagsArray[self::METATAG_FIELD_NAME] = self::ACTION_VALUE;
-      $metaTagsJson = json_encode($metaTagsArray);
-      $entity->field_metatags->value = $metaTagsJson;
-      $entity->save();
-    }
-  }
+  protected static $actionValue = 'disabled';
 
 }
