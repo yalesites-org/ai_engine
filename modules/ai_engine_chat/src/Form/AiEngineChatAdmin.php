c5d1da45791b3c8975e135c26aa9f58310e85e5f
Marc Berger <marc@fourkitchens.com>
2025-02-20 11:17:33 -0800

    feat(IYY-328): Add customizable button text

--- modules/ai_engine_chat/src/Form/AiEngineChatAdmin.php
+++ modules/ai_engine_chat/src/Form/AiEngineChatAdmin.php
@@ -47,6 +47,12 @@ class AiEngineChatAdmin extends ConfigFormBase {
       '#title' => $this->t('Enable floating chat button'),
       '#default_value' => $config->get('floating_button') ?? FALSE,
     ];
+    $form['floating_button_text'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Floating button text'),
+      '#default_value' => $config->get('floating_button_text') ?? $this->t('Ask Yale Chat'),
+      '#required' => TRUE,
+    ];
     $form['azure_base_url'] = [
       '#type' => 'url',
       '#title' => $this->t('Azure base URL'),
@@ -63,6 +69,7 @@ class AiEngineChatAdmin extends ConfigFormBase {
     $this->config(self::CONFIG_NAME)
       ->set('enable', $form_state->getValue('enable'))
       ->set('floating_button', $form_state->getValue('floating_button'))
+      ->set('floating_button_text', $form_state->getValue('floating_button_text'))
       ->set('azure_base_url', $form_state->getValue('azure_base_url'))
       ->save();
     parent::submitForm($form, $form_state);
