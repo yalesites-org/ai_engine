3b03660557c40c4ce0636698215a0940110db03c
David Blankenship <david.blankenship@yale.edu>
2025-08-18 12:12:57 -0400

    fix: update to latest sync'd version

--- modules/ai_engine_chat/react/vite.config.ts
+++ modules/ai_engine_chat/react/vite.config.ts
@@ -1,8 +1,12 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const currentDir = path.basename(process.cwd());
+const isNamedReact = currentDir === 'react';
+
+
+let config = {
   plugins: [react()],
   build: {
     outDir: '../static',
@@ -15,4 +19,32 @@ export default defineConfig({
       '/chat': 'http://localhost:5000'
     }
   }
-})
+};
+
+if (isNamedReact) {
+  config = {
+    base: "/modules/contrib/ai_engine/modules/ai_engine_chat/react/static",
+    plugins: [react()],
+    build: {
+      outDir: "static",
+      emptyOutDir: true,
+      sourcemap: true,
+      rollupOptions: {
+        output: {
+          entryFileNames: `assets/[name].js`,
+          chunkFileNames: `assets/[name].js`,
+          assetFileNames: `assets/[name].[ext]`,
+        },
+      },
+    },
+    server: {
+      proxy: {
+        '/ask': 'http://localhost:5000',
+        '/chat': 'http://localhost:5000'
+      }
+    }
+  }
+}
+
+// https://vitejs.dev/config/
+export default defineConfig(config)
