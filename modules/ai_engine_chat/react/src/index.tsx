0204e0f15cbe7f5516ef27b9068d58a7737e1de2
David Blankenship <david.blankenship@yale.edu>
2025-08-18 11:27:39 -0400

    feat: update to latest yale-development version
    This is a first attempt to keep us in parity with the Consoso project.

--- modules/ai_engine_chat/react/src/index.tsx
+++ modules/ai_engine_chat/react/src/index.tsx
@@ -1,24 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { initializeIcons } from "@fluentui/react";
-import Layout from "./pages/layout/Layout";
-import { AppStateProvider } from "./state/AppProvider";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter, Route, Routes } from 'react-router-dom'
+import { initializeIcons } from '@fluentui/react'
 
-initializeIcons();
+import Chat from './pages/chat/Chat'
+import Layout from './pages/layout/Layout'
+import NoPage from './pages/NoPage'
+import { AppStateProvider } from './state/AppProvider'
+
+import './index.css'
+
+initializeIcons("https://res.cdn.office.net/files/fabric-cdn-prod_20241209.001/assets/icons/")
 
 export default function App() {
   return (
     <AppStateProvider>
-      <Layout />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Chat />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </AppStateProvider>
-  );
+  )
 }
 
-ReactDOM.createRoot(
-  document.getElementById("ai-engine-chat-widget") as HTMLElement
-).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
--- modules/ai_engine_chat/react/vite.config.ts
+++ modules/ai_engine_chat/react/vite.config.ts
@@ -1,26 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/modules/contrib/ai_engine/modules/ai_engine_chat/react/static",
   plugins: [react()],
   build: {
-    outDir: "static",
+    outDir: '../static',
     emptyOutDir: true,
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        entryFileNames: `assets/[name].js`,
-        chunkFileNames: `assets/[name].js`,
-        assetFileNames: `assets/[name].[ext]`,
-      },
-    },
+    sourcemap: true
   },
   server: {
     proxy: {
-      "/ask": "http://localhost:5000",
-      "/chat": "http://localhost:5000",
-    },
-  },
-});
+      '/ask': 'http://localhost:5000',
+      '/chat': 'http://localhost:5000'
+    }
+  }
+})
